1f29c0611e5d50446029b4733c654e84c24fcc23
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2022-11-03 16:01:48 -0400

    Add files via upload

---
+++ Maximo Flujo/Edmonds Karp/edmondsKarp.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h> 
+#define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
+using namespace std; 
+#define MAX_N 101   
+
+int grafo[MAX_N][MAX_N];     
+int path[MAX_N];                
+int nodes;
+int maxFlow(int start,int theend) { 
+    int maxFlow = 0;
+    while(true) { 
+        memset(path, -1, sizeof(path));
+        queue<int> cola;
+        cola.push(start);
+        path[start] = start; 
+        while(!cola.empty() && path[theend]==-1) {
+            int currentNode = cola.front();
+            cola.pop();
+            for(int i = 0 ; i < nodes ; i++) {
+                if(grafo[currentNode][i] > 0 && path[i]==-1)  {  // puedo visitar? 
+                    path[i] = currentNode;                       // guardar camino 
+                    cola.push(i);                                  // agregar a la cola
+                }   
+            }
+        }
+       
+        if(path[theend]==-1) {
+            break;
+        }
+        int minFlow = 1e9;
+        for(int from = path[theend],to = theend; from!=to ; from = path[to = from]) {
+            minFlow = min(minFlow,grafo[from][to]);
+        }
+        
+        for(int from = path[theend],to = theend; from!=to ; from = path[to = from]) {
+            grafo[from][to] -= minFlow; // grafo de capacidad
+            grafo[to][from] += minFlow; // grafo residual
+        }
+        maxFlow += minFlow;
+    }
+    return maxFlow;
+}
+
+
+
+int main() {
+input;
+int  aristas, nroCases;
+cin>>nroCases; 
+while(nroCases--) {
+    cin>>nodes>>aristas;
+    int start,theend; 
+    cin>>start>>theend;
+    memset(grafo,0,sizeof(grafo));
+    for(int i=0;i<aristas;i++) {
+        int from, to, capacity; 
+        cin>>from>>to>>capacity;
+        grafo[from][to] = capacity;          // camino capacidad
+        grafo[to][from] = 0;                //  flujo camino residual 
+    }
+    cout<<maxFlow(start,theend)<<endl;
+    }    
+}
